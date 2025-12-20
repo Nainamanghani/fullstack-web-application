6439d95e0c36bf79a6aacaf867505e6574a856b1
Kunal Tomar <rekha6317r@gmail.com>
2025-12-20 12:36:09 +0530

    feat: add root endpoint to avoid 404 confusion

--- demo/src/main/java/com/example/demo/FliprWebsiteApplication.java
+++ demo/src/main/java/com/example/demo/FliprWebsiteApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@org.springframework.web.bind.annotation.RestController
 public class FliprWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FliprWebsiteApplication.class, args);
+	}
+
+	@org.springframework.web.bind.annotation.GetMapping("/")
+	public String home() {
+		return "Backend is running! Access API at /api/projects";
 	}
 
 }
